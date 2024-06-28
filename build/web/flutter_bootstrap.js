8ae469ae25310b76cb30135e7378581049e0f5bb
Jotham Martin Wambi <64904430+Jotham566@users.noreply.github.com>
2024-06-28 16:47:44 +0900

    Set up GitHub Pages deployment

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3241347246"
+    serviceWorkerVersion: "1939496921"
   }
 });
